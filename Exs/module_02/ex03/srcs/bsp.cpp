c9a8d434cce01b4d8fd9c64c224631495a7fc369
Joao <nascimento.joaogb@gmail.com>
2023-05-01 10:32:42 +0300

    adjustment mod 02

--- Exs/module_02/ex03/srcs/bsp.cpp
+++ Exs/module_02/ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jonascim <jonascim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 13:32:01 by jonascim          #+#    #+#             */
-/*   Updated: 2023/03/24 15:25:30 by jonascim         ###   ########.fr       */
+/*   Updated: 2023/05/01 10:32:10 by jonascim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,23 @@ static Fixed abs(Fixed point)
 	return point;
 }
 
+static bool onSegment(Point const a, Point const b, Point const c)
+{
+	if(a.getX() <= Fixed::max(b.getX(),c.getX())
+		&& a.getX() >= Fixed::min(b.getX(),c.getX())
+		&& a.getY() <= Fixed::max(b.getY(),c.getY())
+		&& a.getY() >= Fixed::max(b.getY(),c.getY()))
+		return (true);
+	return (false);
+}
+
 static Fixed	area(Point const a, Point const b, Point const c)
 {
 	Fixed area;
 
 	area = ((a.getX()*(b.getY() - c.getY())) + (b.getX()*(c.getY() - a.getY())) + (c.getX()*(a.getY() - b.getY())))/2;
+	if (area == 0 && onSegment(a, b, c))
+		return (Fixed(-1));
 	return (area);
 }
 
@@ -35,5 +47,7 @@ bool bsp( Point const a, Point const b, Point const c, Point const point )
 	area2 = abs(area(point, b, c));
 	area3 = abs(area(point, a, c));
 	total_area = abs(area(a, b, c));
+	if (area1 == -1 || area2 == -1 || area3 == -1 || total_area == -1)
+		return (false);
 	return (total_area == area1 + area2 + area3);
 }
